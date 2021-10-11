e34dc402972b8be0735694b1f91c6eefd73eadef
VeigaCoder <gbrl316@gmail.com>
2021-10-11 01:03:25 -0300

    Started doing basic axios requests at pokeapi

--- src/components/pokedex-pokemon-info/index.js
+++ src/components/pokedex-pokemon-info/index.js
@@ -9,34 +9,81 @@ import {
   PokedexPokemonAvatar
 } from '../../elements/pokedex-blocks'
 import 'nes.css/css/nes.min.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios, { Axios } from 'axios'
 
 export const PokedexInfo = () => {
-  const [pokes, setPokes] = useState('pokémon')
-  const [name, setName] = useState('Pokémon')
-  const [photo, setPhoto] = useState('pokémon  photo')
+  const [pokes, setPokes] = useState([])
+  const [name, setName] = useState('Choose a pokémon')
+  const [abilities, setAbilities] = useState([])
+
+  const GetPokemon = async () => {
+    try {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon')
+      setPokes(response.data.results)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const RenderPokemon = (item) => {
+    return (
+      <PokedexPokemonAvatar
+        className='nes-container is-rounded' onClick={() => {
+          setName(item.name)
+          axios.get(`https://pokeapi.co/api/v2/pokemon/${item.name}`)
+            .then(res => {
+              setAbilities(res.data.abilities)
+            })
+        }}
+      >
+        {item.name.toUpperCase()}
+      </PokedexPokemonAvatar>
+    )
+  }
+  const RenderItem = (item) => {
+    return (
+      <TableItem className='nes-container is-rounded'>
+        {item.ability.name.toUpperCase()}
+      </TableItem>
+    )
+  }
+
+  useEffect(() => {
+    GetPokemon()
+  }, [])
 
   return (
     <>
       <PokedexDisplay1 className='nes-container is-rounded'>
         <PokedexPokemonSearcher className='nes-input is-rounded' />
         <PokedexDisplayPokemons className='nes-container is-rounded'>
-          <PokedexPokemonAvatar className='nes-container is-rounded'>pokémon</PokedexPokemonAvatar>
-          <PokedexPokemonAvatar className='nes-container is-rounded'>pokémon</PokedexPokemonAvatar>
+          {pokes.map(RenderPokemon)}
+
         </PokedexDisplayPokemons>
       </PokedexDisplay1>
       <PokedexDisplay2 className='nes-container is-rounded'>
         <Table>
-          <TableItem>
-            Pokémon name: {name}
-          </TableItem>
+          <tr>
+            <TableItem>
+              {name.toUpperCase()}
+            </TableItem>
+          </tr>
+          <tr>
+            <PokedexDisplayPhoto className='nes-container is-rounded' />
+          </tr>
         </Table>
-        <PokedexDisplayPhoto className='nes-container is-rounded'>
-          <img
-            src='https://i.pinimg.com/originals/1b/41/20/1b412053a7b59ab47149a3eb59e5804d.png'
-            width='100%'
-          />
-        </PokedexDisplayPhoto>
+        <Table>
+          <tr>
+            <TableItem>
+              ABILITIES
+            </TableItem>
+          </tr>
+          <tr>
+            {abilities.map(RenderItem)}
+          </tr>
+        </Table>
+
       </PokedexDisplay2>
     </>
   )
