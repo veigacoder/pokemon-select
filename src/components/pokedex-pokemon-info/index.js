fddf1830457ad5196392440a12e9fd2f23d81516
VeigaCoder <gbrl316@gmail.com>
2021-12-24 16:42:20 -0300

    Deleted line that wont let build script run

--- src/components/pokedex-pokemon-info/index.js
+++ src/components/pokedex-pokemon-info/index.js
@@ -1,22 +1,14 @@
 import 'nes.css/css/nes.min.css'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { typeColors } from '../../assets/colors'
-import { getPokemons } from '../../store/actions/pokemonsActions'
 
 export const PokedexInfo = (props) => {
   useEffect(() => {
-    const pokemonsGet = axios.get('https://pokeapi.co/api/v2/pokemon')
+
   }, [])
 
   return (
-    <div>
-      {pokemons.map(p =>
-        <React.Fragment key={p.id}>
-          {p.name}
-        </React.Fragment>
-      )}
-    </div>
+    <div />
   )
 }
 
