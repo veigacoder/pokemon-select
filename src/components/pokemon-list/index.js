909c3b95929da381e4e85187ba65cc6c9ab7ab5b
VeigaCoder <gbrl316@gmail.com>
2022-01-05 00:31:14 -0300

    Changed domains names where pokemons names are displayed, redux integration is now done with hooks

---
+++ src/components/pokemon-list/index.js
@@ -0,0 +1,27 @@
+import 'nes.css/css/nes.min.css'
+import React, { Component, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { DisplayButton, Table } from '../../elements/pokedex-blocks'
+import { actionCreators } from '../../store'
+
+export const Pokemons = () => {
+  const pokemon = useSelector((state) => state.pokemons)
+  const dispatch = useDispatch()
+  const { getPokemons } = bindActionCreators(actionCreators, dispatch)
+
+  useEffect(() => {
+    getPokemons()
+  }, [])
+  return (
+    <Table>
+      {pokemon.map(p =>
+        <DisplayButton>
+          {p.name}
+        </DisplayButton>
+      )}
+    </Table>
+  )
+}
+
+export default Pokemons
